b9f3830b6c01016c63d4229721f8a605f8fb031f
mousumidey-125 <mousumidey125@gmail.com>
2024-02-24 20:00:47 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import './App.css';
 import ShortVideo from './components/ShortVideo';
 import videodata from './data/videodata.json'
+import {useSwipeable} from 'react-swipeable'
 
 function App() {
   const [videoIndex,setVideoIndex]=useState(0)
@@ -17,11 +18,25 @@ function App() {
   const handleNext=()=>{
     {videoIndex===videodata.length-1 ? setVideoIndex(0) : setVideoIndex(videoIndex+1)}
   }
+
+  const handlers=useSwipeable({
+    onSwipedUp:()=>{
+      handleNext()
+    },
+    
+    onSwipedDown:()=>{
+      handlePrev()
+    }
+    
+  })
   return (
 <div className="videoContainer">
 <span class="material-symbols-outlined prevButton" onClick={()=>handlePrev()}>navigate_before</span>
-<div className="video">
-<ShortVideo id={currentVideo.id} src={currentVideo.src} title={currentVideo.title} ></ShortVideo>
+<div className="video" {...handlers}>
+  
+  <ShortVideo id={currentVideo.id} src={currentVideo.src} title={currentVideo.title} ></ShortVideo>
+ 
+
 </div>
 
 <span class="material-symbols-outlined nextButton" onClick={()=>handleNext()}>navigate_next</span>
